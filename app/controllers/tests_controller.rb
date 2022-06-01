a2ab16b34b762f947725a0ba89ac24e8e1b1ce5a
InkaOro <65492226+InkaOro@users.noreply.github.com>
2022-06-01 17:41:06 +0100

    updated code for question card

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,16 @@
+class TestsController < ApplicationController
+  # before_action :set_test, only: [:show]
+
+  def show
+    @questions = Question.first.question_content
+    @answers = Answer.last.answer_content
+  end
+
+  private
+
+  def set_test
+    # @test = Test.find(params[:id])
+    # authorize @test
+  end
+
+end
