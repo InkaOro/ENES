7f83f77042935afc9cb07546769526ef2af4c367
Nathalie Roth <nathaliemiriam.roth@gmail.com>
2022-06-06 17:16:34 +0100

    corrected the percentage

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,12 @@ class Test < ApplicationRecord
   has_many :test_answers
   has_many :test_questions
   has_many :questions, through: :test_questions
+
+  def correct_answers
+    test_answers.count(&:correct)
+  end
+
+  def correct_percentage
+    (correct_answers.to_f / test_questions.count * 100).round
+  end
 end
