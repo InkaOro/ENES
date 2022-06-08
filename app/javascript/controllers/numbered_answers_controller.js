15e6638187f594f56da4cd40335151e24a33ffc6
Nathalie Roth <nathaliemiriam.roth@gmail.com>
2022-06-08 18:26:20 +0100

    severalchanges on profile and result page

---
+++ app/javascript/controllers/numbered_answers_controller.js
@@ -0,0 +1,40 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  connect() {
+    let numbers = ["a) ", "b) ", "c) ", "d) "];
+    let labels = document.querySelectorAll("label") || document.querySelectorAll("li");
+
+    labels.forEach((label) => {
+      let index_option = Array.prototype.indexOf.call(labels, label);
+
+      if(index_option <= 3) {
+        let number = numbers[index_option]
+        label.innerHTML = number +  label.innerHTML
+      } else if(index_option <= 7) {
+        let number = numbers[index_option - 4]
+        label.innerHTML = number +  label.innerHTML
+      } else if(index_option <= 11) {
+        let number = numbers[index_option - 4*2]
+        label.innerHTML = number +  label.innerHTML
+      } else {
+        let number = numbers[index_option - 4*3]
+        label.innerHTML = number +  label.innerHTML
+      }
+
+
+
+    });
+  }
+}
